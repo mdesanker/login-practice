d651ef0c45d929bff80cb6faff4eb4c7e909f07f
Michael Desanker <mdesanker@gmail.com>
2022-01-03 10:25:44 -0500

    Add passport session functions

--- app.js
+++ app.js
@@ -37,6 +37,16 @@ passport.use(
   })
 );
 
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
 app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
